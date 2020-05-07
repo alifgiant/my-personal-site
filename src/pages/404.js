e13eea121421489928aa06b366f4d55c2721b24a
alifgiant <muh.alifgiant@gmail.com>
2020-05-07 11:51:10 +0800

    fixing

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,8 +1,5 @@
 import React from "react"
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
-
 const NotFoundPage = () => (
     <div>
         <h1>NOT FOUND</h1>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,17 @@
 import React from "react"
 import { Link } from "gatsby"
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
-
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
+  <div>
+    {/* <SEO title="Home" /> */}
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+      <img alt="profile" src="https://www.gravatar.com/avatar/6f16a13c13154a4060cd0a9c88e9b078?s=250&d=monsterid&r=g"/>
     </div>
     <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
+  </div>
 )
 
 export default IndexPage
