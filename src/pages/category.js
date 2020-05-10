e904944c2874784eae932f42eb3e3fea743a0850
alifgiant <muh.alifgiant@gmail.com>
2020-05-10 15:13:14 +0800

    remove console log

--- src/pages/category.js
+++ src/pages/category.js
@@ -93,7 +93,6 @@ export default () => {
   );
 
   const { edges: categories } = result;
-  console.log(categories);
 
   let categoriesData = [];
   categories.forEach(({ node: category }) => {
@@ -102,7 +101,6 @@ export default () => {
       slug: category.fields.slug,
     });
   });
-  console.log(categoriesData);
 
   return <CategoryRoll categoriesData={categoriesData} />;
 };
